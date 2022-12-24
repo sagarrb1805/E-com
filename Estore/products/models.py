0a69ceb94e1289953145105b06c14e06d0fe3efa
sagarrb1805 <sagarrb1805@gmail.com>
2022-12-24 12:50:34 +0530

    Added models for products and category

--- Estore/products/models.py
+++ Estore/products/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from .utils import slugify_product_title, calculate_final_price
 
-# Create your models here.
+def product_pre_save(sender, instance, *args, **kwargs):
+    if instance.product_slug == None:
+        slugify_product_title(instance)
+    if instance.product_final_price == None:
+        calculate_final_price(instance)
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=250)
+    category_image = models.ImageField(upload_to='CategoryImages/')
+
+    def __str__(self):
+        return self.category_name
+
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=250)
+    product_description = models.TextField(null=True, blank=True)
+    product_price = models.FloatField()
+    product_discount = models.FloatField(null=True, blank=True)
+    product_final_price = models.FloatField()
+    product_slug = models.SlugField(unique=True, max_length=250, null=True, blank=True)
+    product_category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.product_name
+
+
+pre_save.connect(product_pre_save, sender=Product)
+
+
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_image = models.ImageField(upload_to='ProductImages')
+
+    def __str__(self):
+        return self.product
---
+++ Estore/products/utils.py
@@ -0,0 +1,23 @@
+from django.utils.text import slugify
+import random
+
+
+def slugify_product_title(instance, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.product_name)
+    Klass = instance.__class__
+    qs = Klass.objects.filter(product_slug=slug).exclude(id=instance.id)
+
+    if qs.exists():
+        rand_int = random.randint(100000, 900000)
+        slug = f"{slug}-{rand_int}"
+        return slugify_product_title(instance, new_slug=slug)
+    instance.product_slug = slug
+    return instance
+
+def calculate_final_price(instance):
+    
+    instance.product_final_price = instance.product_price - ((instance.product_price*instance.product_discount)/100)
+    return instance
